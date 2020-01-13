b39333df3488630a9a4250a43675e340013b1887
Dave Baines <bainezydave@gmail.com>
2020-01-13 19:01:11 +1100

    added local passport

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,42 +1,56 @@
+const LocalStrategy = require('passport-local').Strategy;
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 const keys = require('./keys');
 const mongoose = require('mongoose');
 const User = mongoose.model('users');
 
-module.exports = function (passport)
+module.exports = function(passport)
 {
-    passport.use(
-        new GoogleStrategy({
-            clientID: keys.googleAuth.clientID,
-            clientSecret: keys.googleAuth.clientSecret,
-            callbackURL: keys.googleAuth.callbackURL,
-            proxy: true
-        }, (access, refresh, profile, done) =>
-        {        
-            User.findOne({
-                googleID: profile.id
-            })
-            .then(user =>
+    passport.use(new LocalStrategy(
+        function (username, password, done) 
+        {
+            User.findOne({ username }, function (err, user) 
             {
-                if (user)
-                {
-                    done(null, user);
-                } else
-                {
-                    const newUser = {
-                        googleID: profile.id,
-                        firstName: profile.name.givenName,
-                        lastName: profile.name.familyName,
-                        email: profile.emails[0].value
-                    };
-
-                    new User(newUser)
-                    .save()
-                    .then(user => done(null, user));
-                }
+                if (err) return done(err);
+                if (!user) return done(null, false);
+                if (!user.verifyPassword(password)) return done(null, false);
+                return done(null, user);
             });
-        })
-    );
+        }
+    ));
+
+    // passport.use(
+    //     new GoogleStrategy({
+    //         clientID: keys.googleAuth.clientID,
+    //         clientSecret: keys.googleAuth.clientSecret,
+    //         callbackURL: keys.googleAuth.callbackURL,
+    //         proxy: true
+    //     }, (access, refresh, profile, done) =>
+    //     {        
+    //         User.findOne({
+    //             googleID: profile.id
+    //         })
+    //         .then(user =>
+    //         {
+    //             if (user)
+    //             {
+    //                 done(null, user);
+    //             } else
+    //             {
+    //                 const newUser = {
+    //                     googleID: profile.id,
+    //                     firstName: profile.name.givenName,
+    //                     lastName: profile.name.familyName,
+    //                     email: profile.emails[0].value
+    //                 };
+
+    //                 new User(newUser)
+    //                 .save()
+    //                 .then(user => done(null, user));
+    //             }
+    //         });
+    //     })
+    // );
 
     passport.serializeUser((user, done) => done(null, user.id));
     passport.deserializeUser((id, done) => User.findById(id).then(user =>
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,6 +4,8 @@ const passport = require('passport');
 const AuthController = require("../controllers/auth");
 const { isUser, isGuest } = require('../helpers/roles');
 
+
+app.post('/login', passport.authenticate('local', { failureRedirect: '/login' }), function(req, res) { res.redirect('/'); });
 router.get('/google', isGuest, passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'] }));
 router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), AuthController.verify);
 
