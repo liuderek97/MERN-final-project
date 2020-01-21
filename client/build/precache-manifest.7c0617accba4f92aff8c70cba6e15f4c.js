04975225eefb1e3beaad8f74531f9fdf28216826
AidanL <aidanlawson31@gmail.com>
2020-01-21 12:46:12 +1100

    heroku

---
+++ client/build/precache-manifest.7c0617accba4f92aff8c70cba6e15f4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ee9363106600884fe2863fc3f2f4ff5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb11c13f63a07bdd7785",
+    "url": "/static/css/main.16da9702.chunk.css"
+  },
+  {
+    "revision": "26e0d0bb2f8cb44d38f4",
+    "url": "/static/js/2.9939e205.chunk.js"
+  },
+  {
+    "revision": "ae2422ec27b9cb142d4d387903c03a20",
+    "url": "/static/js/2.9939e205.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cb11c13f63a07bdd7785",
+    "url": "/static/js/main.506215a0.chunk.js"
+  },
+  {
+    "revision": "cfac838e1c153daacb16",
+    "url": "/static/js/runtime-main.e9b75281.js"
+  }
+]);
