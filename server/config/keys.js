325399cac82bbe8a43a56629d2d75bd8a2a05b27
AidanL <aidanlawson31@gmail.com>
2020-01-22 12:35:16 +1100

    testing deploy chaged kes.js

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,5 +1,6 @@
 if (process.env.NODE_ENV === 'production') 
 {
+    console.log("PRODUCTION DATABASE")
     module.exports = {
         mongoURI: 'mongodb+srv://aidan:derek@saran-thai-lv7zl.mongodb.net/saran-thai?retryWrites=true&w=majority'
     }
